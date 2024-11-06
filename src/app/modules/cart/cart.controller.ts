349b80703eb5244b86133c0e26a15ec57df77e10
unknown <bijontalukder1247@gmail.com>
2024-11-06 23:03:24 +0600

    cart api

--- src/app/modules/cart/cart.controller.ts
+++ src/app/modules/cart/cart.controller.ts
@@ -1,6 +1,7 @@
 import httpsStatus from 'http-status-codes'
 import { NextFunction, Request, Response } from "express";
 import { cartService } from "./cart.service";
+import mongoose from 'mongoose';
 
 const createCart = async(req:Request,res:Response,next:NextFunction)=>{
     const result = await cartService.createCart(req.body);
@@ -12,6 +13,21 @@ const createCart = async(req:Request,res:Response,next:NextFunction)=>{
       });
     
 }
+const getCartByUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const objectId = new mongoose.Types.ObjectId(req.params.id);
+        const result = await cartService.getCartByUser(objectId);
+        res.status(httpsStatus.OK).json({
+            statusCode: httpsStatus.OK,
+            success: true,
+            message: "Get cart successfully!",
+            data: result,
+        });
+    } catch (error) {
+        next(error); // Forward any errors to the error handling middleware
+    }
+};
 export const cartController={
-    createCart
+    createCart,
+    getCartByUser
 }
--- src/app/modules/cart/cart.routes.ts
+++ src/app/modules/cart/cart.routes.ts
@@ -3,6 +3,7 @@ import { cartController } from "./cart.controller";
 
 const router = expree.Router();
 router.post("/create",cartController.createCart)
+router.get("/user/:id",cartController.getCartByUser)
 
 
 export const CartRouter = router 
