538a662fbb19447cdc9ce2a8b9720b09bcf7f5e2
BijonTalukder <bijontalukder1247@gmail.com>
2024-12-14 12:33:24 +0600

    update

--- src/app/modules/orders/order.route.ts
+++ src/app/modules/orders/order.route.ts
@@ -4,7 +4,7 @@ import { orderController } from "./order.controller";
 const router = express.Router();
 router.post("/create", orderController.createOrder)
 router.get("/", orderController.getAllOrders);
-router.get("/get-order-by-store/:id", orderController.getOrdersByStore)
+router.get("/store/:id", orderController.getOrdersByStore)
 router.get("/user/:id", orderController.getOrderByUser)
 
 export const orderRouter = router 
